b0a741cc4e7080c0a60a339147299fa31af8216c
Samuel Brunner <samuel.brunner@st.oth-regensburg.de>
2025-12-07 21:35:24 +0100

    add comment to postgres extension installer

--- configuration/postgresql/initdb/001-chirpstack_extensions.sh
+++ configuration/postgresql/initdb/001-chirpstack_extensions.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# The script 001-chirpstack_extensions.sh enables specific PostgreSQL extensions required by the ChirpStack application.
+# pg_trgm;: Enables the pg_trgm (Trigram) extension. This is typically used for performing fast text similarity searches (e.g., searching for device names or descriptions).
+# hstore;: Enables the hstore extension. This allows PostgreSQL to store sets of key-value pairs within a single value, which is often used for storing dynamic configuration or tags associated with devices.
+
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$POSTGRES_DB" <<-EOSQL
